3ea71f41188401ed61b0dc158e1be31514217cac
ramrk399 <ramavtarchauhan2000@gmail.com>
2025-06-09 23:39:43 +0530

    Update queue.py

--- AloneX/utils/inline/queue.py
+++ AloneX/utils/inline/queue.py
@@ -72,13 +72,13 @@ def aq_markup(_, chat_id):
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
-        [
+       # [
             #InlineKeyboardButton(
                # text="• 𝐒ᴜᴘᴘᴏʀᴛ •", url=config.SUPPORT_CHAT
            # ),
            # InlineKeyboardButton(
               #  text="• 𝐔ᴘᴅᴀᴛᴇs •", url=config.SUPPORT_CHANNEL
-            ),
+            #),
         ],
         [
             InlineKeyboardButton(
