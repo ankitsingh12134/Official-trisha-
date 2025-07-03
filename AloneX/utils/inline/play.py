3387d0936816f65cff2f359738371c94ae7b1d9b
Jani-Music <hahahsygs395@gmail.com>
2025-07-04 00:09:49 +0530

    Update play.py

--- AloneX/utils/inline/play.py
+++ AloneX/utils/inline/play.py
@@ -65,10 +65,10 @@ def stream_markup_timer(_, chat_id, played, dur):
         ],
         [
             #InlineKeyboardButton(text="◁", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="◁", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="❚❚", callback_data=f"ADMIN Pause|{chat_id}"),
            # InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-            InlineKeyboardButton(text="◁", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
        # ],
         #[
             #InlineKeyboardButton(
@@ -90,10 +90,10 @@ def stream_markup(_, chat_id):
     buttons = [
         [
           #  InlineKeyboardButton(text="❚❚", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="◁", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="❚❚", callback_data=f"ADMIN Pause|{chat_id}"),
             #InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-            InlineKeyboardButton(text="◁", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
        # ],
         #[
             #InlineKeyboardButton(
